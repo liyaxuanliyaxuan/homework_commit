0164cb04b5c74a7f1b65eeee406d98f3f3773952
liyaxuanliyaxuan <15196387158@163.com>
2020-03-30 14:46:42 +0800

    axios

---
+++ web_front_prj/homework_commit/homework-commit-sys/demo/src/axios.js
@@ -0,0 +1,25 @@
+
+import Axios from 'axios'
+import qs from 'qs'
+
+import {Component} from 'react'
+//Axios.defaults.baseURL = 'http://localhost:3000/api'
+if(window.localStorage.getItem('token')){
+  Axios.defaults.headers.common['token'] =  window.localStorage.getItem('token')
+}
+
+
+Component.prototype.$axios=Axios //将axios挂载到Component上，以供全局使用
+Component.prototype.$qs = qs
+
+Axios.interceptors.response.use(function (response) {
+    return response.data;//只获取data数据
+  }, function (error) {
+    if(error.response){
+      switch (error.response.status){
+        case 401:
+          window.location.assign('/#/')
+      }
+      return Promise.reject(error);
+    }  
+  });
